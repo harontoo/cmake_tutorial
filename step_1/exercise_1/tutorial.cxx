bbd1e8d354a549963eee5772edd4537e88339ef1
harontoo <haroncheruiyot254@outlook.com>
2023-11-20 15:11:47 +0300

    minor changes to exercise_1

--- step_1/exercise_1/tutorial.cxx
+++ step_1/exercise_1/tutorial.cxx
@@ -1,23 +1,20 @@
-//A simple program that computes the square root of a number
+
 #include <cmath>
-#include <cstdlib>//TODO 5:Remove this line
+#include <cstdlib>
 #include <iostream>
 #include <string>
 
-//TODO 11: Include TutorialConfig.h
+
 int main(int argc,char* argv[])
 {
-
+//if no input is provided show the user some help on how to interact with the program via cmd/terminal
 	if(argc<2)
 	{
-		//TODO 12: Create a print statement using Tutorial_VERSION_MAJOR
-		//	and Tutorial_VERSION_MINOR
 		std::cout<<"Usage: "<<argv[0]<<" number"<<std::endl;
 		return 1;
 	}
 
 	//Convert input to double
-	//TODO 4: Replace atof(argv1) with std::stod(argv[1])
 	const double inputValue = atof(argv[1]);
 
 	//Calculate square root
