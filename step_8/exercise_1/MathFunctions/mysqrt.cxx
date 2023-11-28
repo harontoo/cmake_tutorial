c762413c6870e8bdc256d8e9f5cbf2f7ca77c2cf
harontoo <haroncheruiyot254@outlook.com>
2023-11-28 11:40:47 +0300

    step_8

---
+++ step_8/exercise_1/MathFunctions/mysqrt.cxx
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "mysqrt.h"
+#include <cmath>
+
+namespace mathfunctions {
+	namespace details {
+		//a hack square root calculations using simple operations.
+		double mysqrt(double x)
+		{
+			//Check if USE_MYMATH library actually gets called
+			std::cout << "Hello World,USE_MYMATH IS ON" << std::endl;
+
+			//Use cmath implementation based on the resultim values of HAVE_LOG and HAVE_EXP
+#if defined(HAVE_LOG) && defined(HAVE_EXP)
+			double result = std::exp(std::log(x) * 0.5);
+			std::cout << "Computing sqrt of " << x << " to be " << result
+				<< " using log and exp" << std::endl;
+			return result;
+#else
+			
+			if (x <= 0)
+			{
+				return 0;
+			}
+
+			double result = x;
+			//do ten iterations.
+			for (int i=1; i <= 10; ++i)
+			{
+				if (result <= 0)
+				{
+					result = 0.1;
+				}
+
+				double delta = x - (result * result);
+				result = result + 0.5 * delta / result;
+				std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
+			}
+			return result;
+#endif
+		}
+	}
+}
